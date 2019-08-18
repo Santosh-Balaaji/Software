95021ca45830205585d30589d0fd5c834f10d734
Santosh Balaaji <sv.santosh2695@gmail.com>
2019-08-19 01:16:53 +1200

    button add

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ router.get("/", function(req,res,next){
     //res.send("INDEX page");
     res.render("index", {title: "EXPRESS MONGO"})
 });
-
+var button = document.createElement("button");
+button.innerHTML = "Student Details";
+button.addEventListener ("click", function() {
+    location.href="client\show.html";
+  });
 module.exports = router;
